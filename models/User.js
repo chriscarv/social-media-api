b42b5b95ce938b2563be2eedf2f4ff0f23e7fe47
christian <100375218+chriscarv@users.noreply.github.com>
2022-07-27 06:55:20 -0400

    user model

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const {Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username:{
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: 'enter valid email',
+            match: [/.+\@.+\..+/]
+        },
+        thoughts: {
+
+        },
+        freinds: {
+            
+        }
+    }
+)
