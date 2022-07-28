38c99559352957762f738b223aad4f6f7d3c2841
christian <100375218+chriscarv@users.noreply.github.com>
2022-07-28 16:21:08 -0400

    user post put and delete controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -27,6 +27,27 @@ const userController = {
             console.log(err);
             res.sendStatus(400);
         });
+    },
+    createUser({body}, res) {
+        User.create(body)
+        .then(dbUserData => res.join(dbUserData))
+        .catch(err => res.json(err));
+    },
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true })
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(404).json({ message: 'no user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({_id: params.id })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
     }
 };
 
